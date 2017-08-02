545f0e18eec4eeee0d8ff77fbf0152a9bc972b98
siscely george <siscely.george@gmail.com>
2017-08-02 09:30:53 -0700

    commit changes

--- scripts/landing.js
+++ scripts/landing.js
@@ -9,17 +9,17 @@ var revealPoint = function(points) {
            points[i].style.transform = "scaleX(1) translateY(0)";
            points[i].style.msTransform = "scaleX(1) translateY(0)";
            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-       }
+          }
     }
-     window.onload = function() {
-       if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
-    }
-        var sellingPoints = document.getElementsByClassName('selling-points')[0];
-        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-       window.addEventListener('scroll', function(event) {
+          window.onload = function() {
+          if (window.innerHeight > 950) {
+          animatePoints(pointsArray);
+          }
+          var sellingPoints = document.getElementsByClassName('selling-points')[0];
+          var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+          window.addEventListener('scroll', function(event) {
           if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray);
-         }
-      });
-    }
+              }
+          });
+        }
