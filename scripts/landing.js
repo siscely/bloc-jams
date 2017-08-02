d3ba4e7229c365f037e7fb38e24b541c53be0746
siscely george <siscely.george@gmail.com>
2017-08-02 09:00:03 -0700

    commit checkpoint-9

--- scripts/landing.js
+++ scripts/landing.js
@@ -22,3 +22,4 @@ var revealPoint = function(points) {
              animatePoints(pointsArray);
          }
       });
+    }
---
+++ scripts/utilities.js
@@ -0,0 +1,17 @@
+var pointsArray = [];
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points){
+
+var forEach = function(points) {
+
+           points[i].style.opacity = 1;
+           points[i].style.transform = "scaleX(1) translateY(0)";
+           points[i].style.msTransform = "scaleX(1) translateY(0)";
+           points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+    }
+}
+
+      animatePoints(pointsArray);
+      animatePoints.forEach(forEach);
