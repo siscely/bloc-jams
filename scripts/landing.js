0c6ba3f97ab89208260a4c69952a3544268834a5
siscely george <siscely.george@gmail.com>
2017-08-03 19:49:03 -0700

    commit changes

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,12 +2,12 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points){
 
-var revealPoint = function(index) {
-  points[index].style.opacity = 1;
-  points[index].style.transform = "scaleX(1) translateY(0)";
-  points[index].style.msTransform = "scaleX(1) translateY(0)";
-  points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-}
+  var revealPoint = function(index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  };
 
   forEach(points, revealPoint);
 };
@@ -15,7 +15,7 @@ var revealPoint = function(index) {
 window.onload = function() {
   if (window.innerHeight > 950) {
       animatePoints(pointsArray);
-    }
+  }
 var sellingPoints = document.getElementsByClassName('selling-points')[0];
 var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 window.addEventListener('scroll', function(event) {
