eb8d71b3eec7d2eb6a240964ffdeb7c671c98c77
siscely george <siscely.george@gmail.com>
2017-08-06 14:29:14 -0700

    commit changes

--- scripts/album.js
+++ scripts/album.js
@@ -131,6 +131,11 @@ songListContainer.addEventListener('mouseover', function(event) {
 console.log(event.target);
 if (event.target.parentElement.className === 'album-view-song-item') {
       event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+      var songItem = getSongItem(event.target);
+
+              if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                  songItem.innerHTML = playButtonTemplate;
+              }
     }
 });
 for (var i = 0; i < songRows.length; i++) {
